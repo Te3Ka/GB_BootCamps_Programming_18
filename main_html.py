8aa6b1781d094fc032c31fd9118a3e59e1b2bcff
Te3Ka_PaynE <Mnement4813@yandex.ru>
2023-03-16 22:22:59 +0300

    рандом в HTML

--- main_html.py
+++ main_html.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template
+from random import randint as rd
 
 app = Flask(__name__)
 
 @app.route('/')
 def main():
-	name = "Ivan"
+	name = rd(0, 1)
 	return render_template("base.html", name=name)
 
 if __name__ == '__main__':
