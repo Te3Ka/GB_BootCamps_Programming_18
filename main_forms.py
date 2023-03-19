64c88f80a4433a1bcf48414ffef6229e369819fb
Te3Ka_PaynE <Mnement4813@yandex.ru>
2023-03-19 20:15:06 +0300

    Страница логина

--- main_forms.py
+++ main_forms.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from LoginForm import LogForm
+from AuthForm import AuthForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'MaX13PaynE'
@@ -22,6 +23,25 @@ def reg():
 		return render_template('registration.html', message='Всё верно')
 
 	return render_template('registration.html', title="Регистрация", form=form)
+
+@app.route('/login')
+def log():
+	form = AuthForm()
+	if form.validate_on_submit():
+		with open('file.txt', 't', encoding='utf-8') as file:
+			data = ' '.join(file.readlines())
+		
+		if form.email.data not in data:
+			return render_template('login.html', form=form, message='Я тебя не знаю')
+		else:
+			for i in data.split():
+				if form.email.data in i:
+					if i.split(';')[-1][:-2] == form.password.data:
+						return render_template('login.html', message=f'Я тебя знаю, {form.name.data}')
+
+
+	return render_template('login.html', form=form)
+
 
 if __name__ == '__main__':
 	app.run()
