61da5812ab0605cab79711848a15091659a8e08f
Te3Ka_PaynE <Mnement4813@yandex.ru>
2023-03-19 19:13:02 +0300

    Формы

--- main_forms.py
+++ main_forms.py
@@ -1,10 +1,20 @@
 from flask import Flask, render_template
+from LoginForm import LogForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'MaX13PaynE'
 
-@app.route
+
+@app.route('/')
 def main():
 	return render_template("main.html")
+
+@app.route('/registration', methods=['GET', 'POST'])
+def reg():
+	form = LogForm()
+	if form.validate_on_submit():
+		pass
+	return render_template('registration.html', title="Регистрация", form=form)
 
 if __name__ == '__main__':
 	app.run()
